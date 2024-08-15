4eceb330e5fc597f7e4713178887f03880262d22
Himanshu Malviya <malviya.himansh@gmail.com>
2024-08-16 01:34:04 +0530

    fix: build error

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
   return (
     <div className="grid place-content-center gap-3 h-screen text-center">
       <h1 className={cn("text-5xl text-zinc-500", instrumentSerif.className)}>404: Not Found</h1>
-      <p className="text-zinc-600">seems like you're lost :(</p>
+      <p className="text-zinc-600">seems like you are lost :(</p>
       <Button onClick={() => window.history.back()} color="green" className="mt-5">{'<- Go back'}</Button>
     </div>
   )
